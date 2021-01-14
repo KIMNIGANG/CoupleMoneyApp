1eea421c5ebc2d1527d5158b2b26f7fc20c933c9
kimnigang <zeus2424797@gmail.com>
2021-01-14 23:37:08 +0900

    api key

--- App.jsx
+++ App.jsx
@@ -2,17 +2,10 @@ import { StausBar } from "expo-status-bar";
 import React from "react";
 
 import * as firebase from "firebase";
-const firebaseConfig = {
-  apiKey: "AIzaSyApve9uxrzfTPA082d2ieVoiTxg-DYkL9c",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
-firebase.initializeApp(firebaseConfig);
+import firebaseConfig from "./service/firebase_config";
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
