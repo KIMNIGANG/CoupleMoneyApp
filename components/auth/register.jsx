48b6058ea23fb8146295ab6c42528385ca8bba1c
kimnigang <zeus2424797@gmail.com>
2021-01-19 21:59:03 +0900

    register style

--- components/auth/register.jsx
+++ components/auth/register.jsx
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
+import { Keyboard, Button, Image, StyleSheet, Text, View } from "react-native";
 import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 
 import firebase from "firebase";
 import "firebase/firestore";
+import { useEffect } from "react";
 
 export const Register = ({ navigation }) => {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    Keyboard.addListener("keyboardDidShow", _keyboardDidShow);
+    Keyboard.addListener("keyboardDidHide", _keyboardDidHide);
+
+    // cleanup function
+    return () => {
+      Keyboard.removeListener("keyboardDidShow", _keyboardDidShow);
+      Keyboard.removeListener("keyboardDidHide", _keyboardDidHide);
+    };
+  });
+
+  const _keyboardDidShow = () => {
+    setShow(false);
+  };
+
+  const _keyboardDidHide = () => {
+    setShow(true);
+  };
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
@@ -33,64 +55,88 @@ export const Register = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.blockContainer}>
-        <View style={styles.inputContainer}>
-          <Image source={require("../../img/name.png")} style={styles.icon} />
-          <TextInput
-            style={styles.input}
-            placeholder="name"
-            onChangeText={(name) => {
-              setName(name);
-            }}
-          />
-        </View>
-        <View style={styles.wall} />
-      </View>
-      <View style={styles.blockContainer}>
-        <View style={styles.inputContainer}>
-          <Image source={require("../../img/email.png")} style={styles.icon} />
-          <TextInput
-            style={styles.input}
-            placeholder="email"
-            onChangeText={(email) => {
-              setEmail(email);
-            }}
-          />
-        </View>
-        <View style={styles.wall} />
-      </View>
-      <View style={styles.blockContainer}>
-        <View style={styles.inputContainer}>
-          <Image
-            source={require("../../img/password.png")}
-            style={styles.icon}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="password"
-            secureTextEntry={true}
-            onChangeText={(password) => {
-              setPassword(password);
-            }}
-          />
-        </View>
-        <View style={styles.wall} />
-      </View>
-
-      <View style={styles.button}>
-        <Button
-          onPress={() => {
-            onSignUp();
-          }}
-          title="Sign Up"
+      <View style={styles.header}>
+        <Image
+          style={styles.headerIcon}
+          source={require("../../img/icon.png")}
         />
       </View>
-      <View style={styles.regText}>
-        <Text>Already have a account?</Text>
-        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-          <Text style={styles.signupText}>Login</Text>
-        </TouchableOpacity>
+      <View style={styles.footer}>
+        <View style={styles.headMessage}>
+          <Text style={styles.headText}>
+            Life's greatest happiness is{"\n"}to be convinced we are loved.
+          </Text>
+          <Text style={styles.headAuthor}>Victor Hugo</Text>
+        </View>
+        <View style={styles.inputs}>
+          <View style={styles.blockContainer}>
+            <View style={styles.inputContainer}>
+              <Image
+                source={require("../../img/name.png")}
+                style={styles.icon}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="name"
+                onChangeText={(name) => {
+                  setName(name);
+                }}
+              />
+            </View>
+            <View style={styles.wall} />
+          </View>
+          <View style={styles.blockContainer}>
+            <View style={styles.inputContainer}>
+              <Image
+                source={require("../../img/email.png")}
+                style={styles.icon}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="email"
+                onChangeText={(email) => {
+                  setEmail(email);
+                }}
+              />
+            </View>
+            <View style={styles.wall} />
+          </View>
+          <View style={styles.blockContainer}>
+            <View style={styles.inputContainer}>
+              <Image
+                source={require("../../img/password.png")}
+                style={styles.icon}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="password"
+                secureTextEntry={true}
+                onChangeText={(password) => {
+                  setPassword(password);
+                }}
+              />
+            </View>
+            <View style={styles.wall} />
+          </View>
+        </View>
+
+        <View style={styles.button}>
+          <Button
+            onPress={() => {
+              onSignUp();
+            }}
+            title="Sign Up"
+          />
+        </View>
       </View>
+      {show && (
+        <View style={styles.regText}>
+          <Text>Already have an account?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Landing")}>
+            <Text style={styles.signupText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
@@ -100,12 +146,54 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#ffd7d7",
+  },
+  header: {
+    width: "100%",
+    flex: 0.5,
+    backgroundColor: "#ffd7d7",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerIcon: {
+    marginTop: "6%",
+    width: 350,
+    height: 350,
+  },
+  headMessage: {
+    width: "100%",
+  },
+  headText: {
+    fontSize: 20,
+    fontFamily: "Georgia",
+    marginTop: "5%",
+    marginLeft: "5%",
+  },
+  headAuthor: {
+    fontFamily: "Georgia",
+    fontSize: 15,
+    marginTop: "3%",
+    marginLeft: "70%",
+  },
+
+  footer: {
+    flex: 0.7,
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: "white",
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    paddingBottom: "30%",
+  },
+  inputs: {
+    marginTop: "15%",
+    width: "100%",
   },
   blockContainer: {
     width: "100%",
     justifyContent: "center",
-    marginTop: 10,
     alignItems: "center",
+    marginTop: "3%",
   },
   inputContainer: {
     flexDirection: "row",
